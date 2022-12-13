706d07dfaf944065a704fcdb139a0c9e0192dac4
Carlos Thompson <Carlos.Thompson@criticalsoftware.com>
2022-12-13 14:00:23 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [endereco, setEndereco] = useState({});
+
+  function handleEndereco(event) {
+    const cep = event.target.value;
+
+    setEndereco({
+      cep
+    });
+
+    if (cep && cep.length === 8) {
+      // obter o cep
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then((response) => response.json()) // converte o dados para o formato json
+        .then((data) => {
+          setEndereco((prevEndereco) => {
+            return {...prevEndereco,
+            bairro: data.bairro,
+            cidade: data.localidade,
+            estado: data.uf,
+          }});
+        });
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input placeholder="Digite o CEP" onChange={handleEndereco} />
+        <ul style={{"list-style": "none" }}>
+          <li>CEP: {endereco.cep}</li>
+          <li>Bairro: {endereco.bairro}</li>
+          <li>Cidade: {endereco.cidade}</li>
+          <li>Estado: {endereco.estado}</li>
+        </ul>
       </header>
     </div>
   );
